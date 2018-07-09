152a9dd5d370a8c5e3b06ef22b9b909d573129d4
unknown <oscglezm@gmail.com>
2018-07-08 21:51:51 -0400

    v1.1

---
+++ Controllers/ContactController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using webApi.Models;
+
+namespace webApi.Controllers
+{
+    public class ContactController : ApiController
+    {
+
+        Contact[] contacts = new Contact[]
+        {
+            new Contact(){Id = 1, FirstName = "Cuquita", LastName = "Matraquilla"}, 
+            new Contact(){Id = 2, FirstName = "Pepito", LastName = "paquetico"}, 
+            new Contact(){Id = 3, FirstName = "Maydita", LastName = "la gordita"}, 
+        };
+
+        // GET api/contact
+        public IEnumerable<Contact> Get()
+        {
+            return contacts;
+        }
+
+        // GET api/contact/5
+        public IHttpActionResult Get(int id)
+        {
+            Contact current = contacts.FirstOrDefault<Contact>(c => c.Id == id);
+
+            if (current == null)
+                return NotFound();
+
+            return Ok(current);
+        }
+
+        // POST api/contact
+        public IEnumerable<Contact> Post([FromBody] Contact newContact)
+        {
+            List<Contact> contactList = contacts.ToList<Contact>();
+
+            newContact.Id = contactList.Count+1;
+            contactList.Add(newContact);
+            contacts = contactList.ToArray();
+
+            return contacts;
+        }
+
+        // PUT api/contact/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/contact/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Models/Contact.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace webApi.Models
+{
+    public class Contact
+    {
+        public int Id { get; set; }
+        public string FirstName  { get; set; }
+
+        public string LastName { get; set; }
+
+        public Contact()
+        {
+            Id = -1;
+            FirstName = "";
+            LastName = "";
+        }
+
+    }
+}
